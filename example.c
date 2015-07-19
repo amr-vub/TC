7ea1acb27ccdef3ea804b44ecd46ac1ebca2c702
Amr <amr@ubuntu.(none)>
2015-07-19 07:10:11 +0200

    Adding an example code file

---
+++ example.c
@@ -0,0 +1,18 @@
+int a;
+int b;
+int c;
+int gg(){
+	// arith exp
+	a = b + c;
+	a = 1 + 3;
+	a = c + 2;
+	a = b - c;
+	a = b * c;
+	a = b / c;
+	a = !b;
+	// boolean exp
+	a = !c;
+	a = b<c;
+	a = b>c;
+	a = b!=4;
+}
